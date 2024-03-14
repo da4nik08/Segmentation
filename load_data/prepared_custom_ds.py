8fac8b97b7e34f48c0766ebea44425d405fa19d4
da4nik08 <illia.cherniavskyi1@nure.ua>
2024-03-14 23:30:26 +0200

    test training

--- load_data/prepared_custom_ds.py
+++ load_data/prepared_custom_ds.py
@@ -1,4 +1,5 @@
 from torch.utils.data import Dataset
+from torchvision.transforms import Resize
 import pandas as pd
 import numpy as np
 import os.path as osp
@@ -6,6 +7,17 @@ import torch
 import torchvision
 import cv2
 
+def Create_mask(labels, img_size):
+    mask = np.zeros(img_size*img_size, np.int8)
+    if len(labels) > 1:
+        np_lbl = np.asarray(labels, dtype=int)
+        for i in range(0, len(np_lbl), 2):
+            # Create a tuple of even and odd numbers
+            start_ind = np_lbl[i]
+            end_ind = np_lbl[i] + np_lbl[i + 1]
+            mask[start_ind:end_ind] = 1
+
+    return mask.reshape((img_size, img_size)).T
 
 class CustomDataset(Dataset):
     def __init__(self, config, img_ids, labels):
@@ -15,6 +27,7 @@ class CustomDataset(Dataset):
         self.mask_path = config['dataset']['mask_path']
         self.labels = labels
         self.img_ids = img_ids
+        self.resizer = Resize([self.new_img_size, self.new_img_size])
 
     def __len__(self):
         return len(self.img_ids)
@@ -22,10 +35,13 @@ class CustomDataset(Dataset):
     def __getitem__(self, idx):
         img_id = self.img_ids[idx]
         lbl = self.labels[idx]
-        
-        mask = torchvision.io.read_image(self.mask_path + "/mask" + img_id)                     # laod mask
+
+        mask = Create_mask(lbl, self.img_size)
+        res_mask = self.resizer(torch.tensor(mask).unsqueeze(0))
         image = cv2.imread(self.img_path + "/" + img_id)
         image = torch.tensor(np.transpose(image, (2, 0, 1)), dtype=torch.float32)
         #image = torchvision.io.read_image(self.img_path + "/" + img_id).to(torch.float32)      # load image
         norm_image = image / 127.5 - 1                                                         # normalization from -1 to 1
-        return norm_image, mask
+        return norm_image, res_mask.type(torch.float32)
+        #return norm_image, torch.tensor(mask, dtype=torch.float32).unsqueeze(0)
+
