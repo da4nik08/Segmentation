c82a18542889f363db5f2a15a21aa3ecc211263a
da4nik08 <illia.cherniavskyi1@nure.ua>
2024-03-16 19:22:22 +0200

    finish recall and precision

--- utilities/RecallAndPrecision.py
+++ utilities/RecallAndPrecision.py
@@ -3,24 +3,24 @@ import torch
 
 
 def TruePositive(actualv, predictedv):
-    ...
+    return np.sum((np.round(predictedv) == 1) & (actualv == 1))
 
 def FalseNegative(actualv, predictedv):
-    ...
+    return np.sum((np.round(predictedv) == 0) & (actualv == 1))
 
 def FalsePositive(actualv, predictedv):
-    ...
+    return np.sum((np.round(predictedv) == 1) & (actualv == 0))
 
 def TrueNegative(actualv, predictedv):
-    ...
+    return np.sum((np.round(predictedv) == 0) & (actualv == 0))
 
-def RecallAndPercisoin(actualv, predictedv):
+def RecallAndPrecision(actualv, predictedv):
     actualv = actualv.numpy(force=True)
     predictedv = predictedv.numpy(force=True)
     TP = TruePositive(actualv, predictedv)
     FN = FalseNegative(actualv, predictedv)
     FP = FalsePositive(actualv, predictedv)
     FN = TrueNegative(actualv, predictedv)
-    recall = TP / (TP + FN)
-    percisoin = TP / (TP + FP)
+    recall = TP / (TP + FN) if TP + FN != 0 else 0
+    percisoin = TP / (TP + FP) if TP + FP != 0 else 0
     return recall, percisoin, (TP, FN, FP, FN)
