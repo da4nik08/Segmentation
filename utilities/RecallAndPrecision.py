2710a6c85c437e53a84d9a699a25ff15fcca51e1
da4nik08 <illia.cherniavskyi1@nure.ua>
2024-03-18 16:33:45 +0200

    + Class Metrics

--- utilities/RecallAndPrecision.py
+++ utilities/RecallAndPrecision.py
@@ -24,3 +24,24 @@ def RecallAndPrecision(actualv, predictedv):
     recall = TP / (TP + FN) if TP + FN != 0 else 0
     percisoin = TP / (TP + FP) if TP + FP != 0 else 0
     return recall, percisoin, (TP, FN, FP, FN)
+
+
+class Metrics():
+    def __init__(self):
+        self.recall_per_batches = 0
+        self.precision_per_batches = 0
+        self.metrics_per_batches = np.array([0, 0, 0, 0])
+
+    def batch_step(self, actualv, predictedv):
+        recall, precision, metrics = RecallAndPrecision(actualv, predictedv)
+        self.recall_per_batches += recall
+        self.precision_per_batches += precision
+        self.metrics_per_batches += np.array(metrics)
+
+    def batch_average(self, num_batches):
+        self.recall_per_batches /= num_batches
+        self.precision_per_batches /= num_batches
+        self.metrics_per_batches /= num_batches
+
+    def get_metrics(self):
+        return self.recall_per_batches, self.precision_per_batches, self.metrics_per_batches
