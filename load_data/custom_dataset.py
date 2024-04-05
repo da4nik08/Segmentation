8133478f9e333c27a79eef15cdbc6383a8554a4a
da4nik08 <illia.cherniavskyi1@nure.ua>
2024-04-05 22:04:16 +0300

    prepare data in advance
    masks and images were croped and saves at the preprocessing scage
    in dataloader i only load ready data

--- load_data/custom_dataset.py
+++ load_data/custom_dataset.py
@@ -4,43 +4,26 @@ import numpy as np
 import os.path as osp
 import torch
 import torchvision
-from torchvision.transforms import Resize
 import cv2
 
-def Create_mask(labels, img_size):
-    mask = np.zeros(img_size*img_size, np.float32)
-    if len(labels) > 1:
-        np_lbl = np.asarray(labels, dtype=int)
-        for i in range(0, len(np_lbl), 2):
-            # Create a tuple of even and odd numbers
-            start_ind = np_lbl[i]
-            end_ind = np_lbl[i] + np_lbl[i + 1]
-            mask[start_ind:end_ind] = 1.0
-
-    return mask.reshape((img_size, img_size)).T
 
 class CustomDataset(Dataset):
-    def __init__(self, config):
+    def __init__(self, config, img_ids, mask_ids):
         self.img_size = config['original_img_size']
         self.new_img_size = config['new_img_size']
         self.img_path = config['dataset']['train_img_path']
-        self.labels = list(pd.read_csv(osp.join(config['dataset']['dir_path'], 
-                                                'train_ship_segmentations_v2.csv'))["EncodedPixels"].fillna('').str.split())
-        self.img_ids = list(pd.read_csv(osp.join(config['dataset']['dir_path'], 
-                                                'train_ship_segmentations_v2.csv'))["ImageId"])
-        self.resizer = Resize([self.new_img_size, self.new_img_size])
+        self.mask_path = config['dataset']['mask_path']
+        self.img_ids = img_ids
+        self.mask_ids = mask_ids
 
     def __len__(self):
         return len(self.img_ids)
 
     def __getitem__(self, idx):
         img_id = self.img_ids[idx]
-        lbl = self.labels[idx]
-        
-        mask = Create_mask(lbl, self.img_size)                         # create mask
-        
-        image = cv2.imread(self.img_path+ "/" + img_id)                # load image
-        res_im = self.resizer(torch.tensor(np.transpose(image, (2, 0, 1)))) # resize image to [3, 256, 256]
-        norm_image = res_im / 127.5 - 1                                # normalization from -1 to 1
-        #return image, mask
-        return norm_image, self.resizer(torch.tensor(mask).unsqueeze(0)) # resize mask
+        mask_id = self.mask_ids[idx]
+        mask = np.load(self.mask_path + "/" + mask_id)                                      # load mask        
+        image = cv2.imread(self.img_path + "/" + img_id)                                    # load image
+        tens_img = torch.tensor(np.transpose(image, (2, 0, 1)), dtype=torch.float32)        # create tensor image [3, 256, 256]
+        norm_image = tens_img / 127.5 - 1                                                   # normalization from -1 to 1
+        return norm_image, torch.tensor(mask, dtype=torch.float32).unsqueeze(0)             # return image, mask
--- preprocessing.py
+++ preprocessing.py
@@ -3,26 +3,42 @@ import pandas as pd
 import os
 import yaml
 import cv2
-import torch
-import torchvision
-from torchvision.transforms import Resize
+import pickle
 import os.path as osp
 from tqdm import tqdm
 from utilities.config_load import load_config
+from utilities.crop_functions import *
+from utilities.pkl import save_pkl
+
 
 def preprocessing():
     CONFIG_PATH = "configs/"
     config = load_config("config.yaml")
 
     train_ship_data = pd.read_csv("dataset/train_ship_segmentations_v2.csv")
-    labels = list(pd.read_csv(osp.join(config['dataset']['dir_path'], 
-                                                'train_ship_segmentations_v2.csv'))["EncodedPixels"].fillna('').str.split())
     img_ids = list(pd.read_csv(osp.join(config['dataset']['dir_path'], 
                                                 'train_ship_segmentations_v2.csv'))["ImageId"])
-    resizer = Resize([config['new_img_size'], config['new_img_size']])
 
-    for img_id in tqdm(img_ids):
+    list_ids = list()
+    for img_id in tqdm(list(set(img_ids))):
+        tmask = np.zeros((config['original_img_size'], config['original_img_size']), np.int8)
+        numb_obj = 0
+        for label in list(train_ship_data[train_ship_data['ImageId']==img_id]['EncodedPixels'].fillna('').str.split()):
+            if len(label) < 2:
+                continue
+            mask = Create_mask(label, config['original_img_size'])
+            tmask = tmask | mask
+            numb_obj += 1
+        
+        if int(np.sum(tmask)) == 0:
+            continue
+    
         image = cv2.imread(config['dataset']['train_img_path'] + "/" + img_id)
-        res_im = resizer(torch.tensor(np.transpose(image, (2, 0, 1))))
-        perm_im = res_im.permute(1, 2, 0)
-        cv2.imwrite(config['dataset']['reshaped_img_path'] + "/" + img_id, np.uint8(perm_im.numpy()))
+        crops_index = crop3x3_mask(tmask, numb_obj)
+        for ind in crops_index:
+            np.save(config['dataset']['mask_path'] + '/' + str(ind) + img_id , crop3x3(tmask, ind, config['new_img_size']))
+            cv2.imwrite(config['dataset']['reshaped_img_path'] + '/' + str(ind) + img_id, 
+                        crop3x3channel(image, ind, config['new_img_size']))
+            list_ids.append(str(ind) + img_id)
+
+    save_pkl('dataset/listofindex.pkl', list_ids)
